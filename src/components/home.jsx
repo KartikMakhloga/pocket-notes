f4b69d60e7e170b7c8a124dd5756a36f093706ac
Kartik Makhloga <kartikmakhoga20@gmail.com>
2024-02-28 14:02:16 +0530

    final commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { IoMdLock } from 'react-icons/io';
+import homeImage from '../assets/home.png';
 
 const Home = () => {
     return (
         <div className='flex flex-col items-center justify-center'>
-            <img className="h-4/6 w-2/5 mt-32" src="..\..\src\assets\home.png" alt="img" />
+            <img className="h-4/6 w-2/5 mt-32" src={homeImage} alt="img" />
             <h1 className='font-bold text-3xl mt-20 mb-4 text-center'>Pocket Notes</h1>
             <h2 className='text-center'>Send and receive messages without keeping your phone online.</h2>
             <h2 className='text-center'>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</h2>
